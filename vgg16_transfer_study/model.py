719ac3ee1ea12a7c6fd927d5e0869f50b8c026b0
fh520pzx <1308758071@qq.com>
2019-06-26 16:54:15 +0800

    两种方法，一种是one_hot类型标签，一种是单个标签

--- vgg16_transfer_study/model.py
+++ vgg16_transfer_study/model.py
@@ -1,33 +1,18 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import transfer_learning
+# import transfer_learning
 from sklearn.preprocessing import LabelBinarizer
 
-codes,labels = transfer_learning.create_data()
+codes = np.load('codes.npy')
+labels = np.load('labels.npy')
 lb = LabelBinarizer()
 lb.fit(labels)
 labels_ves = lb.transform(labels)
-print(labels_ves)
-
-
-input_ = tf.placeholder(tf.float32,[None,codes.shape[1]])
-labels_ = tf.placeholder(tf.int64,[None,labels_ves.shape[1]])
-#添加一层256维的全连接层
-fc = tf.contrib.layers.fully_connected(input_,256)
-#添加一层5维的全连接层
-logits = tf.contrib.layers.fully_connected(fc,3)
-
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=labels_)
-cost = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(0.0001)
-train_step = optimizer.minimize(cost)
-predicted = tf.nn.softmax(logits)
-
-#计算精确度
-correct_pred = tf.equal(tf.argmax(predicted,1),tf.argmax(labels_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
-
+label = []
+for i in labels_ves:
+    label.append(i[0])
+# one_hot=tf.one_hot(label,2)
 
 def get_batch(codes,label):
     input_queue = tf.train.slice_input_producer([codes, label])
@@ -35,21 +20,57 @@ def get_batch(codes,label):
     lable_batch = tf.reshape(lable_batch,[batch_size])
     return codes_batch,lable_batch
 
+def get_logits(images,n_class):
+    with tf.variable_scope('logits') as scope:
+        fc = tf.contrib.layers.fully_connected(images,256)
+        logits = tf.contrib.layers.fully_connected(fc,n_class)
+        tf.summary.scalar(scope.name + '/logits', logits)
+    return logits
+
+def loss(logits,labels):
+    with tf.variable_scope('loss') as scope:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+        # cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+        loss = tf.reduce_mean(cross_entropy)
+        tf.summary.scalar(scope.name + '/loss', loss)
+    return loss
+
+def training(cost,learning_rate):
+    with tf.name_scope('optimiazer'):
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        train_step = optimizer.minimize(cost)
+    return train_step
+
+def accuracy(logits,labels):
+    with tf.variable_scope('accuracy') as scope:
+        # predicted = tf.nn.softmax(logits)
+        # correct_pred = tf.equal(tf.argmax(predicted, 1), tf.argmax(labels,1))
+        # accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        correct = tf.nn.in_top_k(logits, labels, 1)
+        correct = tf.cast(correct, tf.float16)
+        accuracy = tf.reduce_mean(correct)
+        tf.summary.scalar(scope.name + '/accuracy', accuracy)
+    return accuracy
+
 batch_size=20
+cod,lab = get_batch(codes,label)
+# cod,lab = get_batch(codes,one_hot)
+train_logits = get_logits(cod,2)
+train_loss = loss(train_logits,lab)
+train_op = training(train_loss,0.0001)
+train_acc = accuracy(train_logits,lab)
+
+
 with tf.Session() as sess:
     saver = tf.train.Saver()
     sess.run(tf.global_variables_initializer())
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess, coord)
-    cod,lab = get_batch(codes,labels_ves)
     try:
-        for i in range(1000):
-            cod,lab = sess.run([cod,lab])
-            feed = {input_:cod,labels_:lab}
-            loss,_ = sess.run([cost,optimizer],feed_dict=feed)
-            acc = sess.run(accuracy,feed_dict=feed)
+        for i in range(50000):
+            loss,acc = sess.run([train_loss,train_acc])
             if(i%50==0):
-                print(i,loss,accuracy)
+                print(i,loss,acc)
         saver.save(sess, "checkpoints/flowers.ckpt")
     except tf.errors.OutOfRangeError:
         print('done')
